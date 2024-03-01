b8256535dba8e9995c38aaf23027e9585b0568b0
Luis Monteiro <felipemoreno9065@gmail.com>
2024-03-01 00:01:43 -0300

    Router of notifications API added

--- routes/routers.js
+++ routes/routers.js
@@ -1,6 +1,7 @@
 const users = require('../src/views/users_views');
 const stores = require('../src/views/stores_views');
 const products = require('../src/views/products_views');
+const notifications = require('../src/views/notifications_views')
 const express = require('express');
 const app = express();
 
@@ -11,6 +12,7 @@ const Routers = express.Router()
 Routers.use('/users', users);
 Routers.use('/stores', stores);
 Routers.use('/products', products);
+Routers.use('/notifications', notifications);
 
 
 module.exports = Routers;
