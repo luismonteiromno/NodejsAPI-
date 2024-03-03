c7b7848c5afb91e1f102d45a140cf9ecc33c24c6
Luis Monteiro <felipemoreno9065@gmail.com>
2024-03-02 23:30:58 -0300

    Role of users added

--- models/Users.js
+++ models/Users.js
@@ -6,6 +6,19 @@ const User = sequelize.define('User', {
         type: DataTypes.STRING,
         allowNull: false,
     },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.ENUM,
+        values: ['owner', 'employee', 'client'],
+        allowNull: false
+    },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
